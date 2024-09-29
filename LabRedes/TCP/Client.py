415a1c7ed541641ec817e3c1504e538caacc9532
Zuubatman <jvitormorandi@gmail.com>
2024-09-29 00:40:39 -0300

    breadcast, pm and register functions

--- LabRedes/TCP/Client.py
+++ LabRedes/TCP/Client.py
@@ -10,15 +10,20 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 def listen():
     while(True):
         try:
-            response = clientSocket.recv(1024)
-            print ('From Server:', response.decode())
+            response = clientSocket.recv(1024).decode()
+
+            if(response.upper() == 'END'):
+                clientSocket.close()
+                break
+
+            print (response)
 
         except  ():
             if not threading.main_thread().is_alive(): break
         
-def send():
+def sendToServer():
     while(True):
-        command = input("Manda bala pae ")
+        command = input()
         clientSocket.send(command.encode())
 
 def run():
@@ -26,7 +31,7 @@ def run():
     clientSocket.connect((serverName,serverPort))
     print(F"Connected with server: {serverName}")
     threading.Thread(target=listen).start()
-    threading.Thread(target=send).start()
+    threading.Thread(target=sendToServer).start()
 
 run()    
 
--- LabRedes/TCP/Server.py
+++ LabRedes/TCP/Server.py
@@ -13,44 +13,75 @@ serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 
 
-def registerUser(username, addr): 
-    clients.append({'username': username, 'addr': addr})
+def registerUser(username, clientSocket):
+    for c in clients: 
+        if c['username'] == username:  
+            return False 
+            
+    clients.append({'username': username, 'clientSocket': clientSocket})
+    return True
     
-def Client(clientSocket, addr):
+def broadCast(message, user_name):
+    for c in clients:  
+        if c['username'] != user_name:
+            message2 = "Broadcast from: <" + user_name + ">: " + message
+            c['clientSocket'].send(message2.encode())
+            
+def privateMessage(userDestination, user_name, message):
+    for c in clients:  
+        if c['username'] == userDestination:
+            message = "<" + user_name + ">: " + message
+            c['clientSocket'].send(message.encode())
+            
+def client(clientSocket, addr):
+    user_name = None
     while(True):
         try:
             clientSocket.send('Insert a command:'.encode())
             command = clientSocket.recv(1024).decode()
-            if(command):
-                clientSocket.send('Oii'.encode())
 
+            if(command.upper().strip() == 'END'):
+                print(F"Closing connection with {clientSocket.getsockname()}")
+                clientSocket.close()
+                break
+     
+            elif(command.upper().strip() == "REG"):
+                clientSocket.send('Insert username:'.encode())
+                username = clientSocket.recv(1024).decode()
+                register_status = registerUser(username, clientSocket)
+                
+                if(register_status):
+                    user_name = username
+                    clientSocket.send('Register Successful:'.encode())
+                else:
+                    clientSocket.send('Username Already Registered'.encode())
+                
+            elif(command.upper().strip()  == "ALL"):
+                clientSocket.send('Insert message:'.encode())
+                message = clientSocket.recv(1024).decode()
+                broadCast(message, user_name)
+                clientSocket.send('Message Sent'.encode())
+                
+            elif(command.upper().strip()  == "PM"):
+                clientSocket.send('Insert user:'.encode())
+                userDestination = clientSocket.recv(1024).decode()
+                clientSocket.send('Insert Message:'.encode())
+                message = clientSocket.recv(1024).decode()
+                privateMessage(userDestination, user_name, message)
+                
+                
         except  (e):
             if not threading.main_thread().is_alive(): break    
 
 
 def listen():
+    print("Server is Listening...")
     while (True):
-        print("Listening ...")
 
         try:
             clientSocket, addr = serverSocket.accept()
             print(F"Listening socket: {clientSocket.getsockname()}")
-            threading.Thread(target=Client, args=(clientSocket, addr)).start()
-           
-
-            # elif(command.upper() == 'LOG'): 
-            #     clientSocket.send('INSERT USERNAME:'.encode())
-            #     username = clientSocket.recv(1024).decode()
-            #     registerUser(username, addr)
-            #     print('Registrado com sucesso')
-
-            # elif(command.upper() == 'MSG'):
-            #     clientSocket.send('DIGITE A MENSAGEM:'.encode())
-            #     message = clientSocket.recv(1024).decode()
-            #     for client in clients:
-            #         client_addr = client['addr']
-            #         if(client_addr != addr):
-            #             clientSocket.send(message.encode())
+            threading.Thread(target=client, args=(clientSocket, addr)).start()
 
         except  (e):
             if not threading.main_thread().is_alive(): break
